54fa31984a4f4ba69fd79e4f8220f7d25a7ca49f
rndviktor <Vitek_belsutovec@mail.ru>
2016-10-06 17:08:33 +0300

    implemented

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 9_github_trending
+
+Run script: type ' python github_trending.py '
+
+For work script requires APP_ID, which can be obtained by the URL: https://github.com/settings/applications/new
+
+Main purpose this script - to print out top trendy github's repositories
+
+After providing APP_ID script will be able to retrieve list of top 'repositories_count' rated repositories,
+which were created within last 'days_interval' days.
+
+At retrieving data script prints out formatted repository's data.
+
+Format exmple:
+
+'number'. Repository:
+Name:  'repository name'  Forks:  'count forks'  Stars:  'count stars'  Issues:  'count issues'
+Description:  'repository description'
+URL:  'repository url'
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,52 @@
+import requests
+from datetime import datetime, timedelta
+import json
+from itertools import islice
+
+APP_ID = ""
+days_interval = 7
+repositories_count = 20
+login_url = ("https://github.com/login/oauth/authorize?"
+             "client_id={}&scope=user%20public_repo")
+top_created_url = ("https://api.github.com/search/repositories?"
+                   "q=created:>{}&sort=stars&order=desc")
+issues_url = "https://api.github.com/repos/{}/{}/issues"
 
 
 def get_trending_repositories(top_size):
-    pass
+    week_ago = datetime.now() - timedelta(days_interval)
+    string_date = week_ago.strftime("%Y-%m-%d")
+    r = requests.get(top_created_url.format(string_date))
+    data = json.loads(r.text)
+    i = 1
+    for repository in islice(data["items"], top_size):
+        print_repository(i, repository)
+        i += 1
+
+
+def print_repository(number, repository):
+    print("{}. Repository:".format(number))
+    print "Name: ", repository["name"], \
+        " Forks: ", repository["forks_count"], \
+        " Stars: ", repository["stargazers_count"], \
+        " Issues: ", \
+        get_open_issues_amount(repository["owner"]["login"],
+                               repository["name"])
+    print "Description: ", repository["description"]
+    print "URL: ", repository["html_url"]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    r = requests.get(issues_url.format(repo_owner, repo_name))
+    data = json.loads(r.text)
+    return len(data)
+
+
+def login_to_github():
+    requests.get(login_url.format(APP_ID))
+    print('Successfully logged in.')
+
 
 if __name__ == '__main__':
-    pass
+    login_to_github()
+    get_trending_repositories(repositories_count)
