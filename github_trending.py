3bcf56ede8ca15736f113369cd49f9f841490c92
rndviktor <Vitek_belsutovec@mail.ru>
2016-10-10 11:47:33 +0300

    changed for python3.5

--- github_trending.py
+++ github_trending.py
@@ -6,8 +6,8 @@ from itertools import islice
 def get_trending_repositories(top_size):
     date_string = (datetime.now() - timedelta(top_size)).strftime("%Y-%m-%d")
     request_params = {"q": "created:>{}".format(date_string),
-              "sort": "stars",
-              "order": "desc"}
+                      "sort": "stars",
+                      "order": "desc"}
     trending_url = "https://api.github.com/search/repositories"
     data = requests.get(trending_url, params=request_params).json()
     return data["items"]
@@ -17,12 +17,12 @@ def print_repositories(repositories_list, output_length=20):
     index = 1
     for repository in islice(repositories_list, output_length):
         print("{}. Repository:".format(index))
-        print "Name: ", repository["name"], \
-            " Forks: ", repository["forks_count"], \
-            " Stars: ", repository["stargazers_count"], \
-            " Issues: ", repository["open_issues_count"]
-        print "Description: ", repository["description"]
-        print "URL: ", repository["html_url"]
+        print("Name: ", repository["name"],
+              " Forks: ", repository["forks_count"],
+              " Stars: ", repository["stargazers_count"],
+              " Issues: ", repository["open_issues_count"])
+        print("Description: ", repository["description"])
+        print("URL: ", repository["html_url"])
         index += 1
 
 
